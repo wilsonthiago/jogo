c9fb62b0576c38c1cb960ebf9c63228659bd1580
Henrique Arsego <henrique.arsego@estudante.uffs.edu.br>
2019-10-29 20:23:11 -0300

    henrique

--- prog.py/divisao.py
+++ prog.py/divisao.py
@@ -1,4 +1,7 @@
 numero1=int(input('Informe um valor:'))
 numero2=int (input ('Informe o segundo valor:'))
 calc=numero1/numero2
-print (calc)
+cont=0
+while cont < calc:
+	print ("O curso é muito bom(multiplicar o bom x numero do calc")
+	cont+=1
