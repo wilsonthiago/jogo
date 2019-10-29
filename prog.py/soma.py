46bb7efe677ded30beca86016ab7af8229ea7bf3
tedescovinicius <viniciusluistedesco@gmail.com>
2019-10-29 20:23:21 -0300

    tedescovinicius

--- prog.py/soma.py
+++ prog.py/soma.py
@@ -1,4 +1,7 @@
-n1=int(input('Informe um valor:'))
-n2=int (input ('Informe o segundo valor:'))
+n1=int(input("Informe um valor: "))
+n2=int(input("Informe um valor: "))
 calc=n1+n2
-print (calc)
+if calc%2==0:
+	print (calc, 'É primo')
+if calc%2==1:
+	print (calc, 'Não é primo')
