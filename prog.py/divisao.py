668da24d0146734b3492932345d19e791d4e5e71
Henrique Arsego <henrique.arsego@estudante.uffs.edu.br>
2019-10-29 20:11:59 -0300

    Update divisao.py

--- prog.py/divisao.py
+++ prog.py/divisao.py
@@ -1,7 +1,3 @@
-
-n1=int(input('Informe um valor:'))
-n2=int (input ('Informe o segundo valor:'))
-calc=n1+n2
 print (calc)n1=int(input('Informe um valor:'))
 n2=int (input ('Informe o segundo valor:'))
 calc=n1/n2
