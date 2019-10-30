28313aeef307cc8b94a043416f9f949b19aebecf
tedescovinicius <viniciusluistedesco@gmail.com>
2019-10-30 11:46:32 -0300

    tedescovinicius

--- prog.py/soma.py
+++ prog.py/soma.py
@@ -1,7 +1,14 @@
+import math
 n1=int(input("Informe um valor: "))
 n2=int(input("Informe um valor: "))
 calc=n1+n2
-if calc%2==0:
-	print (calc, 'É primo')
-if calc%2==1:
-	print (calc, 'Não é primo')
+i=2
+primo=True
+while i<=math.sqrt(calc):
+	if calc%i==0:
+		primo=False
+	i=i+1
+if primo==True:
+	print (calc, 'é primo')
+else:
+	print(calc,'não é primo')
