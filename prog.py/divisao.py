52e5e0b2dd327d87aab12d9cb43c38003b4720cd
berwanger-vini <viniciusluistedesco@gmail.com>
2019-10-30 17:58:17 -0300

    tedescovinicius

--- prog.py/divisao.py
+++ prog.py/divisao.py
@@ -1,7 +1,22 @@
+import math
 numero1=int(input('Informe um valor:'))
 numero2=int (input ('Informe o segundo valor:'))
 calc=numero1/numero2
-cont=0
-while cont < calc:
-	print ("O curso é muito bom(multiplicar o bom x numero do calc")
-	cont+=1
+calc1=numero1//numero2
+calc2=numero1%numero2
+i=2
+primo=True
+print('Divisão:',calc)
+while i<=math.sqrt(calc):
+	if calc2%i==0:
+		primo=False
+	i=i+1
+if primo==True:
+	print (calc, 'é primo')
+else:
+	print(calc,'não é primo')
+print ('Anteriores de:', calc)
+while calc1 > 0:
+	calc1-=1
+	print (calc1, end=" ")
+print ()
