00dabac40819cf2c6c1701dfd1e215c5975090be
tedescovinicius <viniciusluistedesco@gmail.com>
2019-10-30 11:55:01 -0300

    tedescovinicius

--- prog.py/mult.py
+++ prog.py/mult.py
@@ -2,7 +2,4 @@ n1=int(input("Informe um valor: "))
 n2=int(input("Informe um valor: "))
 calc=n1*n2
 print (calc)
-if calc%2==0:
-	print (calc, 'É primo')
-if calc%2==1:
-	print (calc, 'Não é primo')
+
