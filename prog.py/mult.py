f8db9ab0c48b8567d3a8f5eddb9743994922a3fe
tedescovinicius <viniciusluistedesco@gmail.com>
2019-10-30 16:42:12 -0300

    berwanger-vini

--- prog.py/mult.py
+++ prog.py/mult.py
@@ -1,5 +1,13 @@
 n1=int(input("Informe um valor: "))
 n2=int(input("Informe um valor: "))
 calc=n1*n2
+i=0
+j=0
 print (calc)
-
+while i < n1:
+	i+=1
+	j=0
+	while j < n1:
+		j+=1
+		print (n2,end=" ")
+	print ()
