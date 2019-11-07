fa4efc713a985db79857dcfafed5a9e818c9cbf2
tedescovinicius <viniciusluistedesco@gmail.com>
2019-11-06 22:35:18 -0200

    Atualizado

--- prog.py/subtracao.py
+++ prog.py/subtracao.py
@@ -1,9 +1,11 @@
 import math
 i=2
+j=2
 primo=True
 x=int(input("Digite um valor inteiro: " ))
 y=int(input("Digite um valor inteiro: " ))
 calc= x-y
+resultado=calc
 print('Resultado subtração:',x-y)
 while i<=math.sqrt(calc):
 	if calc%i==0:
@@ -13,9 +15,12 @@ if primo==True:
 	print (calc, 'é primo')
 else:
 	print(calc,'não é primo')
-print ('Anteriores de:', calc)
-while calc > 0:
-	calc-=1
-	print (calc, end=" ")
+print ('Anteriores :')
+while resultado > 2:
+	if resultado%j == 0:
+		primo=False
+	else: 
+		print (resultado, end=" ")
+	resultado-=1
 print ()
 
